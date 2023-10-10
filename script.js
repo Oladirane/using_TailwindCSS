ba14cc2b6d231cf2b89fa955683452fc211a18c1
Emmanuel Oladiran <oladirane@gmail.com>
2023-10-10 09:04:55 +0100

    responsiveness

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ tailwind.config = {
     },
   },
 };
+
+const hamb = document.getElementById("hamburger");
+const list = document.getElementById("liste");
+const listitems = document.querySelectorAll(".list-items");
+
+hamb.addEventListener("click", () => {
+  hamb.classList.toggle("active");
+  list.classList.toggle("active");
+});
+
+listitems.forEach((e) => {
+  e.addEventListener('click', () => {
+    hamb.classList.remove('active')
+    list.classList.remove('active')
+  })
+})
